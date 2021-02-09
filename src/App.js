3f6d4fd892a487028d469542e9743752dd3a9c5e
JessicaThi <jessica.thielemans@hetic.net>
2021-02-09 15:17:10 +0100

    add a few css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import './App.css';
+import imageUpload from './img/image.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content-upload">
+        <h1>Upload your image</h1>
+        <p className="text-3">Files should be Jpeg, Png,...</p>
+        <div className="content-image">
+          <img src={imageUpload} alt="icon for upload" />
+          <p className="text-4">Drag & Drop your image here</p>
+        </div>
+        <p className="text-4">Or</p>
+        <button className="btn">Choose a file</button>
+      </div>
     </div>
   );
 }
