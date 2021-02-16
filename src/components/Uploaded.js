066254693e0c7cb405246e0cbe31dfe7b45b32d0
JessicaThi <jessica.thielemans@hetic.net>
2021-02-16 23:44:30 +0100

    add lotties & style component uploaded" "

--- src/components/Uploaded.js
+++ src/components/Uploaded.js
@@ -1,7 +1,9 @@
 import { React, useState, useEffect} from 'react'
+import Lottie from 'react-lottie'
+import animationData from './../lotties/validation'
 
 
-export default function Ulpoaded({files, API_URL}) {
+export default function Uploaded({files, API_URL}) {
     const [setTextToCopy] = useState(null);
 
     const thumbs = files.map(file => (
@@ -12,15 +14,35 @@ export default function Ulpoaded({files, API_URL}) {
         />
     ));
 
+    const defaultOptions = {
+        loop: false,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
+
     useEffect(() => () => {
         // Make sure to revoke the data uris to avoid memory leaks
         files.forEach(file => URL.revokeObjectURL(file.preview));
       }, [files]);
 
     return (
-        <div>
-            { files.length ? <input type="text" value={`${API_URL}/${files[0].path}`} onClick={() => {setTextToCopy(navigator.clipboard.writeText(`${API_URL}/${files[0].path}`))}} /> : "" } 
+        <div className="content-uploaded">
+            <Lottie 
+                options={defaultOptions}
+                height={50}
+                width={50}
+            />
+            <h1>Successfully uploaded !</h1>
             { thumbs }
+            { files.length ? 
+                <div className="content-link">
+                    <input type="text" value={`${API_URL}/${files[0].path}`}  /> 
+                    <button className="btn" onClick={() => {setTextToCopy(navigator.clipboard.writeText(`${API_URL}/${files[0].path}`))}}>Copy link</button>
+                </div>
+            : "" } 
         </div>
     )
 }
--- src/components/Uploading.js
+++ src/components/Uploading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Uploading({uploadPercentage}) {
     return (
-        <div>
+        <div className="content-uploading">
             <h1>Uploading...</h1>
             { uploadPercentage > 0 ?  <progress id="file" max="100" value={uploadPercentage}>{uploadPercentage}</progress> : "" }
         </div>
